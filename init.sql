c4d2e4b523a919261727e9de94d49418e6fdd08f
Mohamed_Adel_ <moh95_1@yahoo.com>
2021-05-22 04:54:06 +0200

    update sql data

--- init.sql
+++ init.sql
@@ -1,7 +1,9 @@
+DROP view IF EXISTS customer_v;
+
 CREATE VIEW customer_v
 AS
 SELECT id,
        name,
-       substr(phone, instr(phone, '(') + 1, 3) as code,
-       substr(phone, instr(phone, ')') + 2)    as number
+       substr(phone, instr(phone, '(') + 1, 3) as country_code,
+       substr(phone, instr(phone, ')') + 2)    as state
 FROM customer
