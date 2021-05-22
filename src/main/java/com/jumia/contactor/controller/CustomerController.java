2e85354a93288a5d82c31fb78d2fe580688d0527
Mohamed_Adel_ <moh95_1@yahoo.com>
2021-05-22 03:34:23 +0200

    Adding Cross Origin to be access from front-end

--- src/main/java/com/jumia/contactor/controller/CustomerController.java
+++ src/main/java/com/jumia/contactor/controller/CustomerController.java
@@ -8,10 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -19,6 +16,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/customer")
+@CrossOrigin(origins = "http://localhost:4200")
 public class CustomerController {
 
 
